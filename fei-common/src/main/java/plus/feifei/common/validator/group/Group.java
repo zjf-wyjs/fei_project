fc94ece6c4ccea759d351cfd0923fbcfef29667e
zjf-wyjs <1790200889@qq.com>
2021-04-19 09:41:44 +0800

    first commit

--- fei-common/src/main/java/plus/feifei/common/validator/group/Group.java
+++ fei-common/src/main/java/plus/feifei/common/validator/group/Group.java
@@ -7,7 +7,8 @@ import javax.validation.GroupSequence;
 /**
  * 定义校验顺序，如果AddGroup组失败，则UpdateGroup组不会再校验
  *
- * 
+ *
+ * @author haha
  */
 @GroupSequence({AddGroup.class, UpdateGroup.class})
 public interface Group {
