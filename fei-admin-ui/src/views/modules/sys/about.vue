093bcf66917581a31ddfabefb97d2fb6a9e99480
zjf-wyjs <1790200889@qq.com>
2021-04-21 21:49:34 +0800

    增加ckeditor

--- fei-admin-ui/src/views/modules/sys/about.vue
+++ fei-admin-ui/src/views/modules/sys/about.vue
@@ -1,10 +1,47 @@
 <template>
+<div>
 
+  <!-- 工具栏容器 -->
+  <div id="toolbar-container"></div>
+
+  <!-- 编辑器容器 -->
+  <div id="editor" style="height: 1000px">
+    <p>This is the initial editor content.</p>
+  </div>
+</div>
 </template>
 
 <script>
+import CKEditor from '@ckeditor/ckeditor5-build-decoupled-document'
+import '@ckeditor/ckeditor5-build-decoupled-document/build/translations/zh-cn'
 export default {
-  name: 'about'
+  name: 'about',
+  data() {
+    return {
+      editor:null,//编辑器实例
+    }
+  },
+  mounted() {
+    this.initCKEditor()
+  },
+  methods: {
+    initCKEditor () {
+      CKEditor.create(
+        document.querySelector('#editor'), {
+          language: 'zh-cn',
+          ckfinder: {
+            uploadUrl: '/admin/Upload/uploadUrl'
+            //后端处理上传逻辑返回json数据,包括uploaded(选项true/false)和url两个字段
+          }
+        }).then(editor => {
+        const toolbarContainer = document.querySelector('#toolbar-container');
+        toolbarContainer.appendChild(editor.ui.view.toolbar.element);
+        this.editor = editor //将编辑器保存起来，用来随时获取编辑器中的内容等，执行一些操作
+      }).catch(error => {
+        console.error(error);
+      });
+    }
+  }
 }
 </script>
 
