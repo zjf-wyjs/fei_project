1a939ccead18fbc3160a955b12d0eb7a152f94a9
zjf-wyjs <1790200889@qq.com>
2021-04-28 17:21:51 +0800

    关于我们

--- fei-common/src/main/java/plus/feifei/common/entity/AboutEntity.java
+++ fei-common/src/main/java/plus/feifei/common/entity/AboutEntity.java
@@ -1,5 +1,6 @@
 package plus.feifei.common.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Data
 @TableName("tb_about")
 public class AboutEntity {
+    @TableId
     private Integer id;
     private String content;
     private LocalDate updateTime;
